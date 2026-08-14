d84d95bbdd9ec77131fa2f74ea229af951483fd2
Khang Le <ngukhangle@gmail.com>
2026-08-14 15:29:56 +0700

    feat: init router

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("bof")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Couldn't load env file: %s", err)
+	}
+
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "localhost"
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html; charset=utf-8")
+		w.Write([]byte(`
+			<html>
+			<head></head>
+				<body>
+					<h1>Hello world</h1>
+				</body>
+			</html>
+		`))
+	})
+
+	s := &http.Server{
+		Addr:    net.JoinHostPort(host, port),
+		Handler: mux,
+	}
+
+	log.Printf("Running server on %s", s.Addr)
+	log.Fatal(s.ListenAndServe())
 }
