c0094725ed2dc64f9700b0755788c14371bbf965
Khang Le <ngukhangle@gmail.com>
2026-08-22 20:12:05 +0700

    refactor: simplying password hashing method

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,46 +1,17 @@
 package auth
 
 import (
-	"errors"
-
 	"github.com/alexedwards/argon2id"
-	"golang.org/x/crypto/bcrypt"
 )
 
-type Hasher interface {
-	Hash(password string) (string, error)
-	Check(password, hash string) (bool, error)
-}
-
-type BcryptHasher struct{ cost int }
-
-func NewBcryptHasher(cost int) *BcryptHasher {
-	return &BcryptHasher{cost: cost}
-}
-
-func (h *BcryptHasher) Hash(password string) (string, error) {
-	b, err := bcrypt.GenerateFromPassword([]byte(password), h.cost)
-	return string(b), err
-}
-
-func (h *BcryptHasher) Check(password, hash string) (bool, error) {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
-		return false, nil
+func Hash(password string) (string, error) {
+	hashedPassword, err := argon2id.CreateHash(password, argon2id.DefaultParams)
+	if err != nil {
+		return "", err
 	}
-	return err == nil, err
+	return hashedPassword, nil
 }
 
-type Argon2Hasher struct{}
-
-func NewArgon2Hasher() *Argon2Hasher {
-	return &Argon2Hasher{}
-}
-
-func (h *Argon2Hasher) Hash(password string) (string, error) {
-	return argon2id.CreateHash(password, argon2id.DefaultParams)
-}
-
-func (h *Argon2Hasher) Check(password, hash string) (bool, error) {
+func Check(password, hash string) (bool, error) {
 	return argon2id.ComparePasswordAndHash(password, hash)
 }
